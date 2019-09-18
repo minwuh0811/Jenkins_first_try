04c1dfd66ee7ffbc04d11b500b2ce5d72325d050
minwuh081@gmail.com <minwuh081@gmail.com>
2019-09-18 11:12:20 +0200

    error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Syst.out.println("Hello World!!");
 //        System.out.println("Git Poll SCM");
     }
 }
